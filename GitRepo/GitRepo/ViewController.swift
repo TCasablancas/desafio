d4cc57f3270ae7634b05212eb5cd82b45f1504dd
Thiago Augusto Da Costa Silva <thyago_casablancas@hotmail.com>
2020-11-26 21:23:34 -0300

    first commit/proj begin w folders

---
+++ GitRepo/GitRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitRepo
+//
+//  Created by Thyago on 26/11/20.
+//  Copyright © 2020 tcasablancas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
